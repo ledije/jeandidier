169ac177ef3b6bb3ef91141bca18a5836530f02b
ledije <62918872+ledije@users.noreply.github.com>
2020-03-31 00:37:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 ''' Main.py  '''
 
 from quoridor import analyser_commande, afficher_damier_ascii, game
-from api import lister_parties , jouer_coup, initialiser_partie
-
 
 if __name__ == '__main__':
     game()
